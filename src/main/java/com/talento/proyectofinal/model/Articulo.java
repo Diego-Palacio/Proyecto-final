b561daf652b63c87d079115aa64235d3689e0f75
diego-palacio <diego_palacio2015@hotmail.com>
2025-07-08 23:02:57 -0300

    fix pom y clase articulo

--- src/main/java/com/talento/proyectofinal/model/Articulo.java
+++ src/main/java/com/talento/proyectofinal/model/Articulo.java
@@ -1,5 +1,49 @@
 package com.talento.proyectofinal.model;
 
+//import org.springframework.stereotype.Indexed;
+
+import jakarta.persistence.*;
+
 public class Articulo {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Double precio;
+
+    public Articulo(){}
+
+    public Articulo(Long id, String nombre, Double precio){
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+
 }
