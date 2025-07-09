44d5c0470ef64f5c87d1be5326e2372457dd4114
diego-palacio <diego_palacio2015@hotmail.com>
2025-07-08 21:50:16 -0300

    Estructura carpetas

---
+++ src/main/java/com/talento/proyectofinal/controllers/ArticuloController.java
@@ -0,0 +1,21 @@
+package com.talento.proyectofinal.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import java.util.List;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping
+
+public class ArticuloController{
+
+}
+
+
+
+
+
+
